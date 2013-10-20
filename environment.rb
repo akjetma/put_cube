e330c11f0cd8f4e128fc236aed1d805a270410a0
Adam Jetmalani <pleasedoughnutreply@gmail.com>
2013-10-19 21:01:07 -0700

    new relic

--- environment.rb
+++ environment.rb
@@ -5,6 +5,10 @@ Dir["#{ File.dirname(__FILE__) }/filters/*.rb"].each { |file| require file }
 Dir["#{ File.dirname(__FILE__) }/handlers/*.rb"].each { |file| require file }
 Dir["#{ File.dirname(__FILE__) }/helpers/*.rb"].each { |file| require file }
 
+configure :production do
+  require 'newrelic_rpm'
+end
+
 set :server, 'thin'
 set :sockets, []
 
