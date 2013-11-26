4458fa34cb9b562481a7d6999591ee0ef00cf930
Adam Jetmalani <pleasedoughnutreply@gmail.com>
2013-11-25 22:43:28 -0800

    video in the canvas

--- public/js/video.js
+++ public/js/video.js
@@ -1,29 +1,26 @@
 (function () {
-
-  var errorCallback = function (e) { console.log(e); };
-
-  var width = $('#video-container').width();
-  var aspect = 1280/720;
-  var height = width / aspect;
-  $('video').height(height);
-  $('video').width(width);
   
+  var canvas = document.querySelector('canvas');
+  var context = canvas.getContext('2d');
   var video = document.querySelector('video');
-  var constraints = {
-    video:{
-      mandatory: {
-        minWidth: 1280,
-        minHeight: 720
-      }
-    }
-  }
+  var constraints = { video: true };
+  var errorCallback = function (e) { console.log(e); };
+  var draw = function (v, c) {
+    c.drawImage(v, 0, 0);
+    setTimeout(function () { draw(v, c); }, 20);
+  };
 
   navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-
   if (navigator.getUserMedia) {
     navigator.getUserMedia(constraints, function (stream) {
       video.src = window.URL.createObjectURL(stream);
     }, errorCallback);
   }
-    
-})();    
+
+  video.addEventListener('canplay', function (e) {
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    draw(video, context);
+  }, false);
+
+})();
