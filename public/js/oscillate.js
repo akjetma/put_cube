36ac476489f9da5443c1c43d529966c8a9915193
Adam Jetmalani <pleasedoughnutreply@gmail.com>
2013-12-22 06:45:38 -0800

    weird pattern

--- public/js/oscillate.js
+++ public/js/oscillate.js
@@ -1,32 +1,44 @@
-$(document).ready(function () {
 
   var context;
   init();
+
+  function freq (key_index) {
+    return 440 * Math.pow(2, (key_index - 49) / 12.0)
+  }
 
   function init() {
     try {
       window.AudioContext = window.AudioContext || window.webkitAudioContext;
       context = new AudioContext();
-      offsetTime = context.currentTime;
     }
     catch(e) {
       alert('Web Audio API is not supported in this browser');
     }
 
     var height = $('#keys').height();
-    $('#visualization-container').height(height);
+    $('#visualization-container').height(height - 30);
     $('.btn').on('click', function () {
       var frequency = $(this).data('frequency');
       var duration = $(this).data('duration');
-      sequence(frequency, duration, 4, 10);
+      var startTime = context.currentTime;
+      sequence(frequency, duration, 4, 10, startTime);
+    });
+
+    $('#sequence').on('click', function () {
+      var startTime = context.currentTime;
+      for (var i=0; i<12; i++) {
+        var frequency = freq(40 + i);
+        sequence(frequency, 1/8, (i+1)/8, 10, startTime);
+        startTime = startTime + 1;
+      }
     });
   }
 
-  function sequence (frequency, duration, period, count) {
-    var startTime = context.currentTime
+  function sequence (frequency, duration, period, count, startTime) {
+    var offset = startTime;
     for (var i=0; i<count; i++) {
-      new Note(frequency, duration, startTime);
-      startTime = startTime + period;
+      new Note(frequency, duration, offset);
+      offset = offset + period;
     }
   }
 
@@ -38,4 +50,3 @@ $(document).ready(function () {
     this.oscillator.stop(startTime + duration);
   };
 
-});
