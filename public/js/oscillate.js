d0eed4b41499afdea10010c61d79f9318efa890e
Adam Jetmalani <pleasedoughnutreply@gmail.com>
2013-12-22 05:46:17 -0800

    simple sequencer

--- public/js/oscillate.js
+++ public/js/oscillate.js
@@ -1,70 +1,41 @@
 $(document).ready(function () {
 
   var context;
-  var active_voices = {};
-
   init();
 
   function init() {
-    
-    var height = $('#keys').height();
-    $('#visualization-container').height(height);
-
-    $('.btn').on('click', function () {
-      var note = $(this).text();
-
-      if (active_voices[note]) {
-        active_voices[note].stop();
-        delete active_voices[note];
-      } else {
-        var frequency = $(this).data('frequency');
-        var voice = new Voice(frequency);
-        active_voices[note] = voice;
-        voice.start();
-      }
-    });
-
     try {
       window.AudioContext = window.AudioContext || window.webkitAudioContext;
       context = new AudioContext();
+      offsetTime = context.currentTime;
     }
     catch(e) {
       alert('Web Audio API is not supported in this browser');
     }
+
+    var height = $('#keys').height();
+    $('#visualization-container').height(height);
+    $('.btn').on('click', function () {
+      var frequency = $(this).data('frequency');
+      var duration = $(this).data('duration');
+      sequence(frequency, duration, 4, 10);
+    });
+  }
+
+  function sequence (frequency, duration, period, count) {
+    var startTime = context.currentTime
+    for (var i=0; i<count; i++) {
+      new Note(frequency, duration, startTime);
+      startTime = startTime + period;
+    }
+  }
+
+  var Note = function (frequency, duration, startTime) {
+    this.oscillator = context.createOscillator();
+    this.oscillator.frequency.value = frequency;
+    this.oscillator.connect(context.destination);
+    this.oscillator.start(startTime);
+    this.oscillator.stop(startTime + duration);
   };
-
-  var Voice = (function(context) {
-    
-    function Voice (frequency) {
-      this.frequency = frequency;
-      this.oscillators = [];
-    };
-
-    Voice.prototype.start = function() {
-      /* oscillator */
-      var oscillator = context.createOscillator();
-      oscillator.type = oscillator.SINE;
-      oscillator.frequency.value = this.frequency;
-
-      /* amplifier */
-      var amplifier = context.createGain();
-      amplifier.gain.value = 0.3;
-
-      /* connections */
-      oscillator.connect(amplifier);
-      amplifier.connect(context.destination);
-
-      oscillator.start(0);
-      this.oscillators.push( oscillator );
-    };
-
-    Voice.prototype.stop = function() {
-      this.oscillators.forEach( function(oscillator, _) {
-        oscillator.stop(0);
-      });
-    };
-
-    return Voice;
-  }(context));
 
 });
